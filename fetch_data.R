ce071711eb6c44ade9ed2cdd56a01838d0458c25
Jianliang <j.gao@imperial.ac.uk>
2024-11-09 01:13:04 +0000

    Update fetch_data.R

--- fetch_data.R
+++ fetch_data.R
@@ -1,4 +1,8 @@
 ticker <- readLines("myapp/tofetch.txt")
-library(quantmod)
+
+if (!require(quantmod)) {
+  install.packages("quantmod", repos="http://cran.r-project.org")
+  library(quantmod)
+}
 getSymbols(ticker, src = "yahoo", auto.assign = TRUE)
 write.csv(get(ticker), paste0("myapp/",ticker, ".csv"))
