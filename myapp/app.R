45f29330b165181767b7cdfa338da67f80aeb5f0
Jianliang Gao <j.gao@users.noreply.github.com>
2024-11-09 13:16:32 +0000

    updated app with plotly

--- myapp/app.R
+++ myapp/app.R
@@ -1,25 +1,47 @@
 library(shiny)
-library(xts)
-library(quantmod)
+library(plotly)
 
 ui <- fluidPage(
+  tags$head(
+    tags$style(HTML("
+      .plotly {
+        width: 100% !important;
+      }
+    "))
+  ),
   textInput("ticker", "Enter Ticker Symbol:", value = "AAPL"),
   actionButton("submit", "Submit"),
-  plotOutput("stockPlot")
+  plotlyOutput("combinedPlot", width = "100%")
 )
 
 server <- function(input, output, session) {
-  # Use eventReactive to trigger data loading when the submit button is clicked
   stockData <- eventReactive(input$submit, {
     ticker <- input$ticker
     data <- read.csv(paste0("../data/", ticker, ".csv"))
     data$Date <- as.Date(data$Date)
-    xts(data[, -1], order.by = data$Date)
+    data
   })
   
-  output$stockPlot <- renderPlot({
+  output$combinedPlot <- renderPlotly({
     data <- stockData()
-    chartSeries(data, name = input$ticker)
+    ticker <- input$ticker
+    
+    # Create the candlestick plot
+    candlestick <- plot_ly(data, x = ~Date, type = "candlestick",
+                           open = ~data[[paste0(ticker, ".Open")]], 
+                           high = ~data[[paste0(ticker, ".High")]], 
+                           low = ~data[[paste0(ticker, ".Low")]], 
+                           close = ~data[[paste0(ticker, ".Close")]],
+                           name = "Candlestick")
+    
+    # Create the volume plot
+    volume <- plot_ly(data, x = ~Date, y = ~data[[paste0(ticker, ".Volume")]], type = 'bar', name = 'Volume', yaxis = "y2")
+    
+    # Combine the plots
+    subplot(candlestick, volume, nrows = 2, shareX = TRUE) %>%
+      layout(title = paste("Candlestick and Volume Chart for", ticker),
+             yaxis = list(title = "Price"),
+             yaxis2 = list(title = "Volume", overlaying = "y", side = "right"))
   })
 }
 
